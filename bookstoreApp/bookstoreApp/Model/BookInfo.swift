5936c400cce139325a6c288be713912a7bb5a8d8
jeff-0324 <jeffap324@gmail.com>
2024-12-30 11:12:58 +0900

    ✨ [Feat]: BookInfo Model 구현

--- bookstoreApp/bookstoreApp/Model/BookInfo.swift
+++ bookstoreApp/bookstoreApp/Model/BookInfo.swift
@@ -4,4 +4,15 @@
 //
 //  Created by jae hoon lee on 12/29/24.
 //
+import Foundation
 
+struct BookInfo: Codable {
+    let documents: [Infos]
+}
+
+struct Infos: Codable {
+    let authors: [String]?
+    let contents: String?
+    let title: String?
+    let thumbnail : String?
+}
---
+++ bookstoreApp/bookstoreApp/Service/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  bookstoreApp
+//
+//  Created by jae hoon lee on 12/30/24.
+//
+
+import UIKit
+import RxSwift
+
+class NetworkManager {
+    
+    
+    
+}
